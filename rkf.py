7971c19135fac451c321cd702d2775bc1a17404f
tianchenji <jtc0708@126.com>
2019-12-24 14:57:51 -0600

    update rkf

--- rkf.py
+++ rkf.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 
 def rkf_update(A, B, C, Q, R, Sigma, z, xhat, delta):
 	'''
+	robust filtering algorithm for energy constraint
 	rkf_update returns state estimate xhat, ellipsoid shape matrix Sigma and shrinkage delta at time k
 	Inputs: 	A, B, C: system dynamics
 				Q, R: energy constraints of process noise and measurement noise respectively
@@ -37,7 +38,41 @@ def rkf_update(A, B, C, Q, R, Sigma, z, xhat, delta):
 	IS = np.linalg.inv(np.dot(C, np.dot(Sigma_priori, C.T)) + R)
 	delta += np.dot(IM.T, np.dot(IS, IM))
 
-	return (Sigma, xhat, delta)
+	return (Sigma_posteri, xhat, delta)
+
+def rbe_update(A, B, C, Q, R, Sigma, z, xhat, delta):
+	'''
+	robust filtering algorithm for instantaneous constraint
+	rbe_update returns state estimate xhat, ellipsoid shape matrix Sigma and shrinkage delta at time k
+	Inputs: 	A, B, C: system dynamics
+				Q, R: energy constraints of process noise and measurement noise respectively
+				Sigma: a posteri error covariance at time k - 1
+				z: measurement at time k
+				xhat: a posteri state estimate at time k - 1
+				delta: shrinkage at time k - 1
+	Outputs:	Sigma: a posteri error covariance at time k
+				xhat: a posteri state estimate at time k
+				delta: shrinkage at time k
+	'''
+
+	# set filtering parameters
+	beta = 0.5
+	rho  = 0.5
+
+	# update Sigma
+	Sigma_priori  = (1 / (1 - beta)) * np.dot(A, np.dot(Sigma, A.T)) + (1 / beta) * np.dot(B, np.dot(Q, B.T))
+	Sigma_posteri = np.linalg.inv((1 - rho) * np.linalg.inv(Sigma_priori) + rho * np.dot(C.T, np.dot(np.linalg.inv(R), C)))
+
+	# update xhat
+	IM = z - np.dot(C, np.dot(A, xhat))
+	xhat = np.dot(A, xhat) + rho * np.dot(Sigma_posteri, np.dot(C.T, np.dot(np.linalg.inv(R), IM)))
+
+	# update shrinkage delta
+	IS = np.linalg.inv((1 / (1 - rho)) * np.dot(C, np.dot(Sigma_priori, C.T)) + (1 / rho) * R)
+	delta = (1 - beta) * (1 - rho) * delta + np.dot(IM.T, np.dot(IS, IM))
+
+	return (Sigma_posteri, xhat, delta)
+
 
 def rkf_project(Sigma, xhat, delta):
 	'''
@@ -50,7 +85,7 @@ def rkf_project(Sigma, xhat, delta):
 	'''
 
 	# cholesky decomposition of shape matrix
-	L = np.linalg.cholesky(Sigma)
+	L = np.linalg.cholesky(np.linalg.inv(Sigma))
 	x0 = np.array([[0], [0]])
 
 	# state estimates start at index 0
@@ -79,9 +114,9 @@ def rkf_project(Sigma, xhat, delta):
 
 # bivariate example
 n_iter = 50
-x      = np.array([[1.2], [2.7]]) # real value
-Q      = np.array([[2, 0], [0, 2]])
-R      = np.array([[2, 0], [0, 2]])
+x      = np.array([[12], [27]]) # real value
+Q      = np.array([[0.02, 0], [0, 0.02]])
+R      = np.array([[0.02, 0], [0, 0.02]])
 
 xreal           = [0] * n_iter
 xreal[0]        = x
@@ -105,7 +140,7 @@ for i in range(1, n_iter):
 	x1_real[i] = float(xreal[i][0])
 	y = xreal[i] + np.random.uniform(-0.1, 0.1, (2, 1))
 	x1_measurements[i] = float(y[0])
-	(Sigma, xhat, delta) = rkf_update(A, B, C, Q, R, Sigma, y, xhat, delta)
+	(Sigma, xhat, delta) = rbe_update(A, B, C, Q, R, Sigma, y, xhat, delta)
 	(s0_min, s0_max, s1_min, s1_max) = rkf_project(Sigma, xhat, delta)
 	x1_lowerbound[i] = s0_min
 	x1_upperbound[i] = s0_max
